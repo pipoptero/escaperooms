d9315974dbb29a78ad0bf2356a0ef7ff7c37811c
pipoptero <isaacborras@msn.com>
2026-08-26 09:27:39 +0200

    Mejorar logros y ampliar coordenadas del mapa

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'the-vault-v21';
+const CACHE_VERSION = 'the-vault-v22';
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
