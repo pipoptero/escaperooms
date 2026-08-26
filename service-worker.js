c972f941cf55ccc5e1e4b6fecf7a1f0e1d16b374
pipoptero <isaacborras@msn.com>
2026-08-26 20:15:14 +0200

    Mejorar ajustes mapa ubicaciones y logros

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'the-vault-v22';
+const CACHE_VERSION = 'the-vault-v28';
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
