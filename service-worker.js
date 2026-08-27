f96c6e6dbfa62a9b5b741001f29d8a1cb53eb8d2
pipoptero <isaacborras@msn.com>
2026-08-27 07:31:03 +0200

    Corregir proveedor del mapa sin API key

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'the-vault-v28';
+const CACHE_VERSION = 'the-vault-v29';
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
